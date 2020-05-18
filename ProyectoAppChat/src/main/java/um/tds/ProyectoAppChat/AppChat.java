563e0327308570e9398489d74a2d3bd336d2ea94
pedrohlario <pedroh.lariomonte@gmail.com>
2020-05-18 17:15:50 +0200

    Upss mas cosaas

---
+++ ProyectoAppChat/src/main/java/um/tds/ProyectoAppChat/AppChat.java
@@ -0,0 +1,10 @@
+package um.tds.ProyectoAppChat;
+
+public class AppChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProyectoAppChat/src/main/java/um/tds/ProyectoAppChat/CatalogoUsuarios.java
@@ -0,0 +1,5 @@
+package um.tds.ProyectoAppChat;
+
+public class CatalogoUsuarios {
+
+}
